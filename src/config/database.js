c8df4d7c8bc55548110a3483053ab68e1b9285af
Juanesxz <juaneswilli@hotmail.com>
2024-08-14 14:57:55 -0500

    creacion del archivo database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+import { connect } from "mongoose";
+
+
+//funcion autoejecutable de la conexion de kla base de datos
+(async () => {
+  try {
+    const db = await connect(process.env.DB_CNN);
+    console.log("Database connected", db.connection.name);
+  } catch (error) {
+    console.log(error);
+  }
+})();
